fbc199312e2cc9a9279d4ca5f4f1a1f483f863c2
JulianDUSDWHS <738743@student.dusd.net>
2025-01-07 08:36:25 -0800

    Progress
    Finished

--- P2FinalAlvarez/Assets/Scripts/GameControl.cs
+++ P2FinalAlvarez/Assets/Scripts/GameControl.cs
@@ -2,20 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
     public GameObject gameOverText;
+    public Text scoreText;
     public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+
+    private int score = 0;
 
     // Start is called before the first frame update
     void Awake ()
     {
         if (instance == null)
         {
+
             instance = this;
-        } Destroy(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
@@ -27,6 +38,15 @@ public class GameControl : MonoBehaviour
         }
     }
 
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+    }
     public void BirdDied()
     { 
         gameOverText.SetActive(true);
